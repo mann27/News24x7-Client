6f71cdefca06ea3cb95ee4a6162f441f79a4227e
Mann Shah <mannshah2712@gmail.com>
2020-02-16 12:32:44 +0530

    refactoring signup component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 import IndexRoutes from './routes/index.js';
-<<<<<<< HEAD
-import Navbar from './components/NavBar';
-import SignupPage from './components/SignupPage'
-||||||| merged common ancestors
-import Navbar from './components/NavBar';
-=======
 import Navbar from './components/Navbar';
->>>>>>> a46d7880cc6bc47cca119260a8801992c545c905
 
 function App() {
   return (
@@ -20,7 +13,6 @@ function App() {
         <div className="container">
           <IndexRoutes />
         </div>
-        <SignupPage/>
       </Router>
     </div>
   );
--- src/components/SignupPage/index.js
+++ src/components/SignupPage/index.js
@@ -5,69 +5,61 @@ import validate from './validate-yup'
 import getValidationSchema from './getValidationSchema-yup'
 import './signup.css'
 
-const initialValues = {
-  email: '',
-  userName: '',
-  password: '',
-  passwordConfirmation: '',
-  consent: false,
-}
-
 export default function SignupPage() {
   return (
-    
     <Formik
-        initialValues={initialValues}
-    validate={validate(getValidationSchema)}
-    onSubmit={onSubmit}
-      render =   {Signup}
-    />
+      initialValues={{
+        email: '',
+        userName: '',
+        password: '',
+        passwordConfirmation: '',
+        consent: false,
+      }}
+      validate={validate(getValidationSchema)}
+      onSubmit={(values, { setSubmitting, setErrors }) => {
+        setTimeout(() => {
+          console.log('User has been sucessfully saved!', values)
+          setSubmitting(false)
+        }, 2000)
+      }}
+    >
+      {({ isSubmitting, errors, handleChange, handleSubmit }) => (
+        <div className="form">
+          <label className="form-field" htmlFor="email">
+            <span>E-mail:</span>
+            <input name="email" type="email" onChange={handleChange} />
+          </label>
+          <div className="form-field-error">{errors.email}</div>
+
+          <label className="form-field" htmlFor="userName">
+            <span>UserName</span>
+            <input name="userName" type="name" onChange={handleChange} />
+          </label>
+          <div className="form-field-error">{errors.name}</div>
+
+          <label className="form-field" htmlFor="password">
+            <span>Password:</span>
+            <input name="password" type="password" onChange={handleChange} />
+          </label>
+          <div className="form-field-error">{errors.password}</div>
+
+          <label className="form-field" htmlFor="passwordConfirmation">
+            <span>Confirm password:</span>
+            <input name="passwordConfirmation" type="password" onChange={handleChange} />
+          </label>
+          <div className="form-field-error">{errors.passwordConfirmation}</div>
+
+          <label className="form-field" htmlFor="consent">
+            <span>Consent:</span>
+            <input name="consent" type="checkbox" onChange={handleChange} />
+          </label>
+          <div className="form-field-error">{errors.consent}</div>
+
+          <button onClick={handleSubmit}>{isSubmitting ? 'Loading' : 'Sign Up'}</button>
+        </div>
+      )}
+    </Formik>
   )
 }
 
-function Signup(props) {
-  const { isSubmitting, errors, handleChange, handleSubmit } = props
 
-  return (
-    <div className="form">
-      <label className="form-field" htmlFor="email">
-        <span>E-mail:</span>
-        <input name="email" type="email" onChange={handleChange} />
-      </label>
-      <div className="form-field-error">{errors.email}</div>
-
-      <label className="form-field" htmlFor="userName">
-        <span>UserName</span>
-        <input name="userName" type="name" onChange={handleChange} />
-      </label>
-      <div className="form-field-error">{errors.name}</div>
-
-      <label className="form-field" htmlFor="password">
-        <span>Password:</span>
-        <input name="password" type="password" onChange={handleChange} />
-      </label>
-      <div className="form-field-error">{errors.password}</div>
-
-      <label className="form-field" htmlFor="passwordConfirmation">
-        <span>Confirm password:</span>
-        <input name="passwordConfirmation" type="password" onChange={handleChange} />
-      </label>
-      <div className="form-field-error">{errors.passwordConfirmation}</div>
-
-      <label className="form-field" htmlFor="consent">
-        <span>Consent:</span>
-        <input name="consent" type="checkbox" onChange={handleChange} />
-      </label>
-      <div className="form-field-error">{errors.consent}</div>
-
-      <button onClick={handleSubmit}>{isSubmitting ? 'Loading' : 'Sign Up'}</button>
-    </div>
-  )
-}
-
-function onSubmit(values, { setSubmitting, setErrors }) {
-  setTimeout(() => {
-    console.log('User has been sucessfully saved!', values)
-    setSubmitting(false)
-  }, 2000)
-}
