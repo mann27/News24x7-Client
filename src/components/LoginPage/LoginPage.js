01551e3c5a812b170edd0fc0a65acbf3d256c2ab
Donika Juthani <39000402+Donika1999@users.noreply.github.com>
2020-02-19 01:12:47 +0530

    Create LoginPage.js

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="email">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    console.log("Submitting");
+    console.log(this.state);
+  };
+}
