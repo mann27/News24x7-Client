fa8dadc0bd81a32f7517a0a4e67450ce8481d616
Donika Juthani <39000402+Donika1999@users.noreply.github.com>
2020-03-10 12:58:52 +0530

    Update index.js

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Formik } from "formik";
 import * as Yup from "yup";
 import '../SignupPage/signup.css'
+const MIN_PASSWORD_LENGTH = 6;
+
 
 
 class LoginPage extends Component {
@@ -17,11 +19,11 @@ class LoginPage extends Component {
                 }}
                 validationSchema={Yup.object().shape({
                     email: Yup.string()
-                        .email()
-                        .required("Required"),
+                        .email('E-mail is not valid!')
+                        .required('E-mail is required!'),
                     password: Yup.string()
-                        .required("No password provided.")
-                        .min(6, "Password is too short - should be 6 chars minimum.")
+                        .required('Password is required!')
+                        .min(MIN_PASSWORD_LENGTH, `Password has to be longer than ${MIN_PASSWORD_LENGTH} characters!`)
                         .matches(/(?=.*[0-9])/, "Password must contain a number.")
                 })}
             >
@@ -36,6 +38,7 @@ class LoginPage extends Component {
                         handleSubmit
                     } = props;
                     return (
+                        <div className="form">
                         <form onSubmit={handleSubmit}>
                             <label htmlFor="email">Email</label>
                             <input
@@ -48,7 +51,7 @@ class LoginPage extends Component {
                                 className={errors.email && touched.email && "error"}
                             />
                             {errors.email && touched.email && (
-                                <div className="input-feedback">{errors.email}</div>
+                                <div className="form-field-error">{errors.email}</div>
                             )}
                             <label htmlFor="email">Password</label>
                             <input
@@ -61,12 +64,12 @@ class LoginPage extends Component {
                                 className={errors.password && touched.password && "error"}
                             />
                             {errors.password && touched.password && (
-                                <div className="input-feedback">{errors.password}</div>
+                                <div className="form-field-error">{errors.password}</div>
                             )}
                             <button type="submit" disabled={isSubmitting}>
                                 Login
           </button>
-                        </form>
+                        </form></div>
                     );
                 }}
             </Formik>
