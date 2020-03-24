504a0a3c9bdbbb3b0cb878819f002666497d40e2
Mann Shah <mannshah2712@gmail.com>
2020-03-24 17:34:39 +0530

    auth issue in createPost

--- src/components/HomePage/CreatePost/index.js
+++ src/components/HomePage/CreatePost/index.js
@@ -2,91 +2,109 @@ import React, { Component } from 'react'
 import './createPostStyle.css'
 import TextAreaFieldGroup from './TextAreaFieldGroup';
 import TextFieldGroup from './TextFieldGroup';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class CreatePost extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-          url:'',
-          text:'',
-          errors: {}
-        };
-        this.onChange = this.onChange.bind(this);
-         this.onSubmit = this.onSubmit.bind(this);
-      }
+  constructor() {
+    super();
+    this.state = {
+      url: '',
+      text: '',
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-      onSubmit(e) {
-        e.preventDefault();
-        
-        if(!this.props.auth.isAuthenticated) {
-          this.props.history.push('/HomePage');
-        }
-    
-        const { user } = this.props.auth;
-    
-        const newPost = {
-          url: this.state.url,
-    //      handle: this.state.handle,
-          text: this.state.text,
-          name: user.name,
-            
-        };
-        console.log(this.props.auth);
-    
-        this.props.addPost(newPost);
-        this.setState({ text: ''});
-        this.setState({url: ''});
-      }
+  onSubmit(e) {
+    e.preventDefault();
 
-      onChange(e) {
-        this.setState({ [e.target.name]: e.target.value });
-      }
-    
+    if (!this.props.auth.isAuthenticated) {
+      this.props.history.push('/');
+    }
 
-    render() {
-        const { errors } = this.state;
-        return (
-        <div className="post-form mb-3 shadow rounded mt-4">
+    const { user } = this.props.auth;
+
+    const newPost = {
+      url: this.state.url,
+      //      handle: this.state.handle,
+      text: this.state.text,
+      name: user.name,
+
+    };
+    console.log(this.props.auth);
+
+    this.props.addPost(newPost);
+    this.setState({
+      text: '',
+      url: ''
+    });
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+
+  render() {
+    const { user: { authenticated } } = this.props
+    const { errors } = this.state;
+    return (
+      <div className="post-form mb-3 shadow rounded mt-4">
         <div className="card card-info">
           <div className="card-header bg-info text-white">Say Somthing...</div>
-          <div className="card-body">
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-              <TextFieldGroup
-                  placeholder="URL"
-                  name="url"
-                  type="url"
-                  value={this.state.url}
-                  onChange={this.onChange}
-                  error={errors.url}
-                />
-                <TextAreaFieldGroup 
-                  placeholder="Thoughts"
-                  name="text"
-                  value={this.state.text}
-                  onChange={this.onChange}
-                  error={errors.text}
-                />
-                <TextFieldGroup
-                  placeholder="Add Tags"
-                  name="text"
-                  type="text"
-                  value={this.state.text}
-                  onChange={this.onChange}
-                  error={errors.text}
-                />
-              </div>
-             
-              <button>
-                Submit
+          {authenticated ? (
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <TextFieldGroup
+                    placeholder="URL"
+                    name="url"
+                    type="url"
+                    value={this.state.url}
+                    onChange={this.onChange}
+                    error={errors.url}
+                  />
+                  <TextAreaFieldGroup
+                    placeholder="Thoughts"
+                    name="text"
+                    value={this.state.text}
+                    onChange={this.onChange}
+                    error={errors.text}
+                  />
+                  <TextFieldGroup
+                    placeholder="Add Tags"
+                    name="text"
+                    type="text"
+                    value={this.state.text}
+                    onChange={this.onChange}
+                    error={errors.text}
+                  />
+                </div>
+
+                <button>
+                  Submit
               </button>
-            </form>
-          </div>
+              </form>
+            </div>
+          ) : <p> <Link to="/login">login</Link>  or <Link to="/signup">signup</Link> to create the post</p>}
         </div>
       </div>
-        )
-    }
+    )
+  }
 }
 
-export default CreatePost;
+CreatePost.propTypes = {
+  user: propTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(CreatePost);
