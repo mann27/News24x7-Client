d260509a82dc9a8d4bb9c2f2047fc5cff52e2ee7
Donika Juthani <39000402+Donika1999@users.noreply.github.com>
2020-04-06 00:23:09 +0530

    updates (#30)
    * Create userstyle.css
    
    * Update index.js
    
    * Update postStyle.css
    
    * Update index.js
    
    * Create ReportButton.js
    
    * Update postDetailsStyle.css
    
    * Update index.js

--- src/components/HomePage/Post/PostDetails/index.js
+++ src/components/HomePage/Post/PostDetails/index.js
@@ -102,7 +102,7 @@ class PostDetails extends Component {
                     <Grid container spacing={1}>
                         <Grid item={true} xs={8}>
                             {!uiloading ? (
-                                <Paper>
+                                <Paper style= {{paddingLeft: '10px', paddingRight:'10px'}}>
                                     <Grid container spacing={3} className="postshape">
                                         <Grid item={true} xs={3} >
                                             <img src={userImage} alt="user" className="usershape" />
@@ -117,7 +117,7 @@ class PostDetails extends Component {
                                                     <p>{dayjs(createdAt).fromNow()}</p>
                                                 </div>
                                                 <div className="tags">
-                                                    <p>{tags}</p>
+                                                    <p style={{ marginLeft: '20px', fontSize: 'medium' }}><u>{tags}</u></p>
                                                 </div>
                                             </div>
                                         </Grid>
