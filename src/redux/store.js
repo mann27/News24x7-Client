181577c04dbc061c6439a424f6acb43d8c764b82
Mann Shah <mannshah2712@gmail.com>
2020-06-26 05:52:35 +0530

    removed redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -21,8 +21,7 @@ const store = createStore(
     reducers,
     initialState,
     compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middleware)
     )
 );
 
