59ca3c9d4088367f2389f9fd2a43ddabc1c38ab9
Shubh1815 <59529068+Shubh1815@users.noreply.github.com>
2020-10-01 14:20:20 +0530

    add home link in 404 page

--- src/components/404Page/index.js
+++ src/components/404Page/index.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import './404.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './404.css';
 
 export default function InvalidPage(){
     return (
         <div className='wrapper'>
             <div>
                 <div className="status-code">404</div>
-                <p className="msg">Sorry! The Page You're Looking For Was Not Found.</p>
+                <div className='pos'>
+                    <p className="msg">Sorry! The Page You're Looking For Was Not Found.</p>
+                    <Link to='/'><button>Go To Home</button></Link>
+                </div>
             </div>
         </div>
     )
