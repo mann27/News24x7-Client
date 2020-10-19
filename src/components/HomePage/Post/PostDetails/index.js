59a6d311b6293849ea4592ee895e43f6cf12bf8d
rohan2734 <jagandevaki1@gmail.com>
2020-10-19 20:51:03 +0530

    added a reusable component

--- src/components/HomePage/Post/PostDetails/index.js
+++ src/components/HomePage/Post/PostDetails/index.js
@@ -17,6 +17,7 @@ import Comment from '../../Comment';
 import './postDetailsStyle.css'
 import spin from '../../../../utils/black_spinner.gif';
 import LikeButton from '../LikeButton';
+import ReusablePost from  '../ReusablePost';
 
 
 class PostDetails extends Component {
@@ -27,11 +28,20 @@ class PostDetails extends Component {
             commentbody: '',
             postId: '',
             errors: {},
-            copyText: ''
+            copyText: '',
+            reported:false,
+
         }
         this.handleOnChange = this.handleOnChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.copyToClipBoard = this.copyToClipBoard.bind(this);
+        this.handlereport = this.handlereport.bind(this);
+
+    }
+    handlereport(e) {
+        this.setState({
+            reported: true
+        })
     }
 
     UNSAFE_componentWillReceiveProps(nextProps) {
@@ -78,17 +88,26 @@ class PostDetails extends Component {
     }
 
     copyToClipBoard = () => {
+        console.log("clicked copytoclipborad");
         const el = window.location.href + `/post/${this.props.data.post.postId}`
         copy(el);
         this.setState({
             copyText: 'Copied!'
         })
+        // 
     }
 
     render() {
+
+        const paperStyle={
+            paddingLeft: '10px', 
+            paddingRight: '10px', 
+            paddingBottom: '10px' 
+        }
         const { user: { authenticated } } = this.props;
         dayjs.extend(relativeTime)
-        const { data: { post: { userImage, handleName, title, body, likeCount, commentCount, createdAt, tags, postId, postImage } } } = this.props;
+        //const { data: { post: { userImage, handleName, title, body, likeCount, commentCount, createdAt, tags, postId, postImage } } } = this.props;
+        const post = this.props.data.post;
         const { data: { post: { comments } } } = this.props;
         const commentMarkUp = comments ? (
             comments.map((comment) =>
@@ -98,56 +117,123 @@ class PostDetails extends Component {
 
         const uiloading = this.props.ui.loading;
         return (
-            <div style={{ backgroundColor: '#08c', minHeight: 'calc(100vh - 138px)' }}>
+            <div style={{ backgroundColor:"rgba(238,238,238)", minHeight: 'calc(100vh - 138px)' }}>
                 <Container maxWidth="md" style={{ paddingTop: '30px' }}>
                     <Grid container spacing={1}>
                         <Grid item={true} xs={8}>
                             {!uiloading ? (
-                                <Paper style={{ paddingLeft: '10px', paddingRight: '10px' }}>
-                                    <Grid container spacing={3} className="postshape">
-                                        <Grid item={true} xs={3} >
-                                            <Grid container spacing={3}>
-                                                <Grid item xs={4}>
-                                                    <img src={userImage} alt="user" className="usershape" />
-                                                </Grid>
-                                                <Grid item xs={8}>
-                                                    <p className="handle"> {handleName} </p>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
-                                        <Grid item={true} xs={9}  >
-                                            <div className="title">
-                                                <b style={{ marginBottom: '0px' }}>{title}</b>
-                                            </div>
-                                            <div className="time-tags">
-                                                <div className="time">
-                                                    <p>{dayjs(createdAt).fromNow()}</p>
-                                                </div>
-                                                <div className="tags">
-                                                    <p style={{ marginLeft: '20px', fontSize: 'medium' }}><u>{tags}</u></p>
-                                                </div>
-                                            </div>
-                                        </Grid>
-                                    </Grid>
-                                    <center>
-                                        {postImage ? <img src={postImage} alt="img" className="postimage" /> : null}
-                                    </center>
-                                    <div style={{ paddingTop: '20px', paddingBottom: '10px' }}>
-                                        <a href={body} className="postbody">{body}</a>
-                                    </div>
-                                    <div className="like-share-comment-bookmark">
-                                        <LikeButton key={postId} postId={postId} />
-                                        <p>{likeCount} likes</p>
+                                // <Paper style={{ paddingLeft: '10px', paddingRight: '10px' }}>
+                                //     <Grid container spacing={3} className="postshape">
+                                //         <Grid item={true} xs={3} >
+                                //             <Grid container spacing={3}>
+                                //                 <Grid item xs={4}>
+                                //                     <img src={userImage} alt="user" className="usershape" />
+                                //                 </Grid>
+                                //                 <Grid item xs={8}>
+                                //                     <p className="handle"> {handleName} </p>
+                                //                 </Grid>
+                                //             </Grid>
+                                //         </Grid>
+                                //         <Grid item={true} xs={9}  >
+                                //             <div className="title">
+                                //                 <b style={{ marginBottom: '0px' }}>{title}</b>
+                                //             </div>
+                                //             <div className="time-tags">
+                                //                 <div className="time">
+                                //                     <p>{dayjs(createdAt).fromNow()}</p>
+                                //                 </div>
+                                //                 <div className="tags">
+                                //                     <p style={{ marginLeft: '20px', fontSize: 'medium' }}><u>{tags}</u></p>
+                                //                 </div>
+                                //             </div>
+                                //         </Grid>
+                                //     </Grid>
+                                //     <center>
+                                //         {postImage ? <img src={postImage} alt="img" className="postimage" /> : null}
+                                //     </center>
+                                //     <div style={{ paddingTop: '20px', paddingBottom: '10px' }}>
+                                //         <a href={body} className="postbody">{body}</a>
+                                //     </div>
+                                //     <div className="like-share-comment-bookmark">
+                                //         <LikeButton key={postId} postId={postId} />
+                                //         <p>{likeCount} likes</p>
 
-                                        <ChatIcon style={{ marginLeft: '15px', marginRight: '0px' }} />
-                                        <p style={{ marginLeft: '10px' }}>{commentCount} Comments</p>
-                                        {
-                                            authenticated && this.showDelete() ? <span className="delete-btn"><button type="button" onClick={this.onClickDelete}>delete</button></span> : null
-                                        }
-                                        {this.state.copyText ? <p>{this.state.copyText}</p> : <span style={{cursor: 'pointer', color: '#08c'}} onClick={this.copyToClipBoard}><ShareIcon style={{ marginLeft: '15px', marginRight: '0px' }} /></span>}
-                                        <ReportIcon />
-                                    </div>
-                                </Paper>
+                                //         <ChatIcon style={{ marginLeft: '15px', marginRight: '0px' }} />
+                                //         <p style={{ marginLeft: '10px' }}>{commentCount} Comments</p>
+                                //         {
+                                //             authenticated && this.showDelete() ? <span className="delete-btn"><button type="button" onClick={this.onClickDelete}>delete</button></span> : null
+                                //         }
+                                //         {this.state.copyText ? <p>{this.state.copyText}</p> : <span style={{cursor: 'pointer', color: '#08c'}} onClick={this.copyToClipBoard}><ShareIcon style={{ marginLeft: '15px', marginRight: '0px' }} /></span>}
+                                //         <ReportIcon />
+                                //     </div>
+                                // </Paper>
+                                // <Paper style={{  paddingLeft: '10px', paddingRight: '10px', paddingBottom: '10px' }}>
+                                //     {/* <Link to={redirect} className="link"> */}
+                                //         <Grid container direction="row" justify="flex-start" style={{padding:'10px 10px',backgroundColor:'#E0EFFE'}}>
+                                //             {/* user image and handle name*/}
+                                //             <Grid xs={1} justify="flex-start">
+                                //                 <img src={userImage} alt="user" className="usershape" />
+                                //             </Grid>
+                                //             <Grid sx={6} direction="column" justify="flex-start" style={{paddingLeft:'10px',display:"flex"}}>
+                                //                 <p className="handle" style={{color:'#001272',fontSize:'20px',fontWeight:400,marginTop:'-10px'}}> {handleName} </p>
+                                //                 <p style={{ marginTop:'-10px',opacity:'70%',fontSize:'10px'}}>{dayjs(createdAt).fromNow()}</p>
+                                //             </Grid>
+                                //         </Grid>
+                                //         <hr style={{opacity:'30%'}}/>
+                                //         <Grid contianer direction="row" >
+                                //             {/* post title*/ }
+                                //             <p style={{ fontFamily: 'Piazzolla',textAlign:"center",fontSize:'30px',fontWeight:800,color:'#001794' }}>{title}</p>
+                                //         </Grid>
+                                //     {/* </Link> */}
+                                //     {/* Image */}
+                                //     <div style={{textAlign:"center"}}>
+                                //         {postImage ? <img src={postImage} style={{width:'100%'}}alt="img" className="postimage" /> : null}
+                                //     </div>
+                                //     {/* tags */}
+                                //     <Grid container direction="row" justify="space-between">
+                                //         <a href={body} style={{marginLeft:'20px'}}>Link</a>
+                                //         <p style={{ fontSize: 'medium', color: '#3351F3' ,marginRight:'20px'}}><u>{tags}</u></p>
+                                //     </Grid>
+                                //     {/* likes and comments count */}
+                                //     <Grid container direction="row" spacing={4} alignItems="center">
+                                //         <div style={{display:"flex"}}>
+                                //             <LikeButton />
+                                //             <p style={{fontSize:'14px',alignSelf:"center"}}>{likeCount} </p>
+                                //         </div>
+                                //         <div style={{display:"flex"}}>
+                                //             <ChatIcon style={{marginLeft:'10px'}}/>
+                                //             <p style={{fontSize:'14px',alignSelf:"center"}}>{commentCount}</p>
+                                //         </div>
+                                        
+                                //         <div style={{display:"flex"}}>
+                                //             {
+                                //                 authenticated && this.showDelete() ? <span className="delete-btn"  ><button type="button" onClick={this.onClickDelete}>delete</button></span> : null
+                                //             }
+                                //         </div>
+                                //     </Grid>
+                                //     <hr style={{opacity:'50%'}}/>
+                                //     {/* like,share ,comment,report symbols */}
+                                //     <Grid container spacing={3}>
+                                //         <Grid item={true} xs={12} >
+                                //             <div className="like-share-comment-bookmark">
+                                //                 <LikeButton key={postId} postId={postId} />
+                                //                 <ChatIcon style={{ marginLeft: '15px', marginRight: '0px' }} />
+                                //                 {this.state.copyText ? <p style={{ fontSize: 'medium', color: '#3351F3' ,marginRight:'20px',marginBottom:"5px"}}>{this.state.copyText}</p> : <span style={{cursor: 'pointer', color: '#08c'}} onClick={this.copyToClipBoard}><ShareIcon style={{ marginLeft: '15px', marginRight: '0px' }} /></span>}
+                                //                 {this.state.reported ? <p style={{ fontSize: 'medium', color: '#3351F3' ,marginRight:'20px',marginBottom:"5px"}}>reported!</p> : <ReportIcon onClick={this.handlereport} />}
+                                //             </div>
+                                //         </Grid>
+                                //     </Grid>
+                                // </Paper >
+                                <ReusablePost
+                                    authenticated={authenticated}  paperStyle={paperStyle}
+                                    post={post} copyText={this.state.copyText} reported={this.state.reported}  
+                                    handleOnChange={() => this.handleOnChange()}
+                                    handlereport={() => this.handlereport()}
+                                    handleSubmit={() => this.handleSubmit()}
+                                    showDelete={() => this.showDelete()}
+                                    onClickDelete={() => this.onClickDelete()}
+                                    copyToClipBoard={() => this.copyToClipBoard()}
+                                    />
                             ) : (
                                     <Paper style={{ padding: '30px' }}>
                                         <center>
--- src/components/HomePage/Post/index.js
+++ src/components/HomePage/Post/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
-import { Paper, Grid } from '@material-ui/core'
+//import { Paper, Grid } from '@material-ui/core'
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import ChatIcon from '@material-ui/icons/Chat';
-import ShareIcon from '@material-ui/icons/Share';
+//import ChatIcon from '@material-ui/icons/Chat';
+//import ShareIcon from '@material-ui/icons/Share';
 // import BookmarkIcon from '@material-ui/icons/Bookmark';
-import ReportIcon from '@material-ui/icons/Report';
+//import ReportIcon from '@material-ui/icons/Report';
 import copy from "copy-to-clipboard";
 
 import './postStyle.css'
-import { Link } from 'react-router-dom'
-import LikeButton from './LikeButton';
+//import { Link } from 'react-router-dom'
+//import LikeButton from './LikeButton';
+import ReusablePost from './ReusablePost';
 
 class Post extends Component {
 
@@ -43,57 +44,72 @@ class Post extends Component {
     }
 
     render() {
+
+        const paperStyle={ 
+            marginTop: '25px', 
+            paddingLeft: '10px', 
+            paddingRight: '10px', 
+            paddingBottom: '10px' 
+        }
         dayjs.extend(relativeTime)
-        let { userImage, handleName, title, body, likeCount, commentCount, createdAt, tags, postId, postImage } = this.props.post;
-        const redirect = `/post/${this.props.post.postId}`;
+        //let { userImage, handleName, title, body, likeCount, commentCount, createdAt, tags, postId, postImage } = this.props.post; //will be passed as props
+        const redirect = `/post/${this.props.post.postId}`; //will be passed as props
         return (
-            <Paper style={{ marginTop: '25px', paddingLeft: '10px', paddingRight: '10px', paddingBottom: '10px' }}>
-                <Link to={redirect} className="link">
-                    <Grid container direction="row" justify="flex-start" style={{padding:'10px 10px',backgroundColor:'#E0EFFE'}}>
-                        {/* user image and handle name*/}
-                        <Grid xs={1} justify="flex-start">
-                            <img src={userImage} alt="user" className="usershape" />
-                        </Grid>
-                        <Grid sx={6} direction="column" justify="flex-start" style={{paddingLeft:'10px',display:"flex"}}>
-                            <p className="handle" style={{color:'#001272',fontSize:'20px',fontWeight:400,marginTop:'-10px'}}> {handleName} </p>
-                            <p style={{ marginTop:'-10px',opacity:'70%',fontSize:'10px'}}>{dayjs(createdAt).fromNow()}</p>
-                        </Grid>
-                    </Grid>
-                    <hr style={{opacity:'30%'}}/>
-                    <Grid contianer direction="row" >
-                        {/* post title*/ }
-                        <p style={{ fontFamily: 'Piazzolla',textAlign:"center",fontSize:'30px',fontWeight:800,color:'#001794' }}>{title}</p>
-                    </Grid>
-                </Link>
-                {/* Image */}
-                <div style={{textAlign:"center"}}>
-                    {postImage ? <img src={postImage} style={{width:'100%'}}alt="img" className="postimage" /> : null}
-                </div>
-                {/* tags */}
-                <Grid container direction="row" justify="space-between">
-                    <a href={body} style={{marginLeft:'20px'}}>Link</a>
-                    <p style={{ fontSize: 'medium', color: '#3351F3' ,marginRight:'20px'}}><u>{tags}</u></p>
-                </Grid>
-                {/* likes and comments count */}
-                <Grid container direction="row"  justify="flex-start" alignItems="center"> 
-                    <LikeButton />
-                    <p style={{fontSize:'14px'}}>{likeCount} </p>
-                    <ChatIcon style={{marginLeft:'10px'}}/>
-                    <p style={{fontSize:'14px'}}>{commentCount}</p>
-                </Grid>
-                <hr style={{opacity:'50%'}}/>
-                {/* like,share ,comment,report symbols */}
-                <Grid container spacing={3}>
-                    <Grid item={true} xs={12} >
-                        <div className="like-share-comment-bookmark">
-                            <LikeButton key={postId} postId={postId} />
-                            <ChatIcon style={{ marginLeft: '15px', marginRight: '0px' }} />
-                            {this.state.copyText ? <p>{this.state.copyText}</p> : <span style={{cursor: 'pointer', color: '#08c'}} onClick={this.copyToClipBoard}><ShareIcon style={{ marginLeft: '15px', marginRight: '0px' }} /></span>}
-                            {this.state.reported ? <p>reported!</p> : <ReportIcon onClick={this.handlereport} />}
-                        </div>
-                    </Grid>
-                </Grid>
-            </Paper >
+                // {/* <Paper style={{ marginTop: '25px', paddingLeft: '10px', paddingRight: '10px', paddingBottom: '10px' }}>
+                //     <Link to={redirect} className="link">
+                //         <Grid container direction="row" justify="flex-start" style={{padding:'10px 10px',backgroundColor:'#E0EFFE'}}>
+                //             {/* user image and handle name*/}
+                //             <Grid xs={1} justify="flex-start">
+                //                 <img src={userImage} alt="user" className="usershape" />
+                //             </Grid>
+                //             <Grid sx={6} direction="column" justify="flex-start" style={{paddingLeft:'10px',display:"flex"}}>
+                //                 <p className="handle" style={{color:'#001272',fontSize:'20px',fontWeight:400,marginTop:'-10px'}}> {handleName} </p>
+                //                 <p style={{ marginTop:'-10px',opacity:'70%',fontSize:'10px'}}>{dayjs(createdAt).fromNow()}</p>
+                //             </Grid>
+                //         </Grid>
+                //         <hr style={{opacity:'30%'}}/>
+                //         <Grid contianer direction="row" >
+                //             {/* post title*/ }
+                //             <p style={{ fontFamily: 'Piazzolla',textAlign:"center",fontSize:'30px',fontWeight:800,color:'#001794' }}>{title}</p>
+                //         </Grid>
+                //     </Link>
+                //     {/* Image */}
+                //     <div style={{textAlign:"center"}}>
+                //         {postImage ? <img src={postImage} style={{width:'100%'}}alt="img" className="postimage" /> : null}
+                //     </div>
+                //     {/* tags */}
+                //     <Grid container direction="row" justify="space-between">
+                //         <a href={body} style={{marginLeft:'20px'}}>Link</a>
+                //         <p style={{ fontSize: 'medium', color: '#3351F3' ,marginRight:'20px'}}><u>{tags}</u></p>
+                //     </Grid>
+                //     {/* likes and comments count */}
+                //     <Grid container direction="row"  justify="flex-start" alignItems="center"> 
+                //         <LikeButton />
+                //         <p style={{fontSize:'14px'}}>{likeCount} </p>
+                //         <ChatIcon style={{marginLeft:'10px'}}/>
+                //         <p style={{fontSize:'14px'}}>{commentCount}</p>
+                //     </Grid>
+                //     <hr style={{opacity:'50%'}}/>
+                //     {/* like,share ,comment,report symbols */}
+                //     <Grid container spacing={3}>
+                //         <Grid item={true} xs={12} >
+                //             <div className="like-share-comment-bookmark">
+                //                 <LikeButton key={postId} postId={postId} />
+                //                 <ChatIcon style={{ marginLeft: '15px', marginRight: '0px' }} />
+                //                 {this.state.copyText ? <p>{this.state.copyText}</p> : <span style={{cursor: 'pointer', color: '#08c'}} onClick={this.copyToClipBoard}><ShareIcon style={{ marginLeft: '15px', marginRight: '0px' }} /></span>}
+                //                 {this.state.reported ? <p>reported!</p> : <ReportIcon onClick={this.handlereport} />}
+                //             </div>
+                //         </Grid>
+                //     </Grid>
+                // </Paper > */}
+                <ReusablePost  
+                    post={this.props.post} paperStyle={paperStyle}
+                    redirect={redirect}
+                    copyText={this.copyText} reported={this.reported}
+                    handlereport={() => this.handlereport()}
+                    copyToClipBoard={() => this.copyToClipBoard()} 
+                    />
+                    
         )
     }
 }
